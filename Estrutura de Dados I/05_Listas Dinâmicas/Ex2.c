41ddf08047dc84ad0b74ce3c70080f165f7be100
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-06-08 10:43:08 -0300

    Add files via upload

---
+++ Estrutura de Dados I/05_Listas Dinâmicas/Ex2.c
@@ -0,0 +1,43 @@
+//Lista dinâmica em forma de filas
+
+//Estrutura nó
+typedef truct NO{
+	int dado;
+	struct NO* prox;
+}NO;
+
+//Estrutura fila
+typedef struct FILA{
+	NO* inicio;
+	NO* fim;
+}FILA;
+
+//Criando variável tipo fila
+FILA *f;
+
+//Enfileirando
+void enfileirar(){
+	NO* ptr = (NO *) malloc(sizeof(NO));	//alocando dinamicamente
+	ptr->dado =  rand()100% 	// o dado do ponteiro recebe um número aleatório
+	ptr->prox = NULL
+	if(f->inicio == NULL) {
+		f->inicio = ptr;
+	} else{
+		f->fim->prox = prt;
+	}
+	f->fim = ptr;
+}
+
+//Desenfileirando
+void desenfileirar(){
+	NO* ptr = f->inicio;	
+	int dado;	//criando variável local de backup
+	if(ptr != NULL) {
+		f->inicio = ptr->prox;
+		ptr->prox = NULL;
+		dado = ptr->dado;
+		free(ptr);
+		return dado;
+	} 
+}
+
